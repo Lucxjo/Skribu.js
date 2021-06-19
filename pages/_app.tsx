ddd378d5604e974b91ab0e82f41c2fc2b7a7a963
Ludoviko <me@ludoviko.xyz>
2021-06-19 22:22:45 +0100

    Added basic auth with google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/nav'
+import { Toaster } from 'react-hot-toast'
 
 function App({ Component, pageProps }: AppProps) {
   return <>
-    <Navbar></Navbar>
+    <Navbar />
     <Component {...pageProps} />
+    <Toaster />
   </>
 }
 export default App
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+import { auth, googleAuthProvider } from "../lib/firebase";
+
 export default function Auth({}) {
+    const user = null
+    const username = null
     return (
         <main>
-            <h1>Sign up</h1>
+            <h1>Sign In</h1>
+            { user?
+                !username ? <UsernameForm /> : <SignOutButton />
+                :
+                <SignInButton />
+            }
         </main>
     )
 }
+
+function SignInButton({}) {
+    const signInWithGoogle = async () => {
+        await auth.signInWithPopup(googleAuthProvider);
+    }
+
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <Image src={'/google.png'} width="90px" height="90px" alt="Google sign in"/> <span className="btn-google">Sign in with Google</span>
+        </button>
+    )
+}
+
+function SignOutButton() {return <button onClick={() => auth.signOut()}>Sign Out</button>;}
+
+function UsernameForm() {return null}
