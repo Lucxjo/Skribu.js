93d4675a7ba0415a0fe503a14e7cfa5100601e83
Ludoviko <me@ludoviko.xyz>
2021-07-09 19:43:25 +0100

    Added functionality to upload posts

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,85 @@
 /** @format */
-import AuthCheck from "../../components/AuthCheck";
+import { useCollection } from 'react-firebase-hooks/firestore';
+import AuthCheck from '../../components/AuthCheck';
+import { auth, firestore, serverTimestamp } from '../../lib/firebase';
+import PostFeed from '../../components/PostFeed';
+import { PostProps } from '../../lib/post';
+import { useRouter } from 'next/dist/client/router';
+import { ChangeEvent, FormEvent, useContext, useState } from 'react';
+import { UserContext } from '../../lib/context';
+import { kebabCase } from 'lodash';
+import toast from 'react-hot-toast';
 
 export default function AdminPage({}) {
 	return (
 		<main>
 			<AuthCheck>
-                <div></div>
-            </AuthCheck>
+				<PostList />
+				<CreateNewPost />
+			</AuthCheck>
 		</main>
+	);
+}
+
+function PostList() {
+	const ref = firestore.collection('users').doc(auth.currentUser?.uid).collection('posts');
+	const query = ref.orderBy('createdAt');
+	const [querySnapshot] = useCollection(query);
+
+	// @ts-ignore
+	const posts: PostProps = querySnapshot?.docs.map(doc => doc.data());
+
+	return (
+		<>
+			<h1>Manage your posts</h1>
+			<PostFeed posts={posts} admin />
+		</>
+	);
+}
+
+function CreateNewPost() {
+	const router = useRouter();
+	const { username } = useContext(UserContext);
+	const [title, setTitle] = useState('');
+	const slug = encodeURI(kebabCase(title));
+
+	const isValid = title.length > 3 && title.length < 100;
+
+	const createPost = async (e: FormEvent) => {
+		e.preventDefault();
+		const uid = auth.currentUser?.uid;
+		const ref = firestore.collection('users').doc(uid).collection('posts').doc(slug);
+		const data = {
+			title,
+			slug,
+			uid,
+			username,
+			published: false,
+			content: '# Hello World!',
+			createdAt: serverTimestamp(),
+			updatedAt: serverTimestamp(),
+			heartCount: 0,
+		};
+
+		await ref.set(data);
+		toast.success('Post created!');
+		router.push(`/admin/${slug}`);
+	};
+
+	return (
+		<form onSubmit={createPost}>
+			<input
+				className="input"
+				value={title}
+				onChange={e => setTitle(e.target.value)}
+				placeholder="Post title"
+			/>
+			<p>
+				<strong>Slug:</strong> {slug}
+			</p>
+			<button type="submit" disabled={!isValid} className="btn-green">
+				Create New Post
+			</button>
+		</form>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,9 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Loader from '../components/loader';
 import { InferGetServerSidePropsType } from 'next';
-import { firestore, postToJson } from '../lib/firebase';
+import { firestore, fromMillis, postToJson } from '../lib/firebase';
 import { useState } from 'react';
 import PostFeed from '../components/PostFeed';
-import firebase from 'firebase';
 
 //Max posts per page
 const LIMIT = 1;
@@ -20,7 +19,7 @@ export const getServerSideProps = async () => {
 		.orderBy('createdAt', 'desc')
 		.limit(LIMIT);
 
-  const posts = (await postsQuery.get()).docs.map(postToJson);
+	const posts = (await postsQuery.get()).docs.map(postToJson);
 
 	return {
 		props: { posts },
@@ -31,39 +30,39 @@ export default function Home(props: any) {
 	const [posts, setPosts] = useState(props.posts);
 	const [loading, setLoading] = useState(false);
 
-  const [postsEnd, setPostsEnd] = useState(false);
-  
-  const getMorePosts = async () => {
-    setLoading(true)
-    const last = posts[posts.length - 1]
-    const cursor = typeof last.createdAt === 'number' ? firebase.firestore.Timestamp.fromMillis(last.createdAt) : last.createdAt
+	const [postsEnd, setPostsEnd] = useState(false);
 
-    const query = firestore
+	const getMorePosts = async () => {
+		setLoading(true);
+		const last = posts[posts.length - 1];
+		const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+		const query = firestore
 			.collectionGroup('posts')
 			.where('published', '==', true)
 			.orderBy('createdAt', 'desc')
-      .startAfter(cursor)
-      .limit(LIMIT)
-    
-    const newPosts = (await query.get()).docs.map(postToJson)
+			.startAfter(cursor)
+			.limit(LIMIT);
 
-    setPosts(posts.concat(newPosts))
-    setLoading(false)
+		const newPosts = (await query.get()).docs.map(postToJson);
 
-    if (newPosts.length < LIMIT) {
-      setPostsEnd(true)
-    }
-  }
+		setPosts(posts.concat(newPosts));
+		setLoading(false);
+
+		if (newPosts.length < LIMIT) {
+			setPostsEnd(true);
+		}
+	};
 
 	return (
-    <main>
-      <PostFeed posts={posts} />
+		<main>
+			<PostFeed posts={posts} />
 
-      {!loading && !postsEnd && <button onClick={getMorePosts}>Show more</button>}
+			{!loading && !postsEnd && <button onClick={getMorePosts}>Show more</button>}
 
-      <Loader show={loading} />
+			<Loader show={loading} />
 
-      {postsEnd && 'You have reached the end!'}
-    </main>
+			{postsEnd && 'You have reached the end!'}
+		</main>
 	);
 }
